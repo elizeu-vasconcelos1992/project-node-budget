1ea45d3adf8cb86d023858028941df16370de087
unknown <elizeucosta21@gmail.com>
2023-02-08 02:09:36 -0300

    refactor: added product types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
 export type User = {
-  id?: number;
-  name?: string;
-  tax?: number;
+  id: number;
+  name: string;
+  tax: number;
+};
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
 };
