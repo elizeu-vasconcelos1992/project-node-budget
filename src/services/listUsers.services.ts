7602197df9bddfdc6495eefb9677113daa722175
unknown <elizeucosta21@gmail.com>
2023-02-08 02:06:55 -0300

    refactor: added handleErrors function

--- src/services/listUsers.services.ts
+++ src/services/listUsers.services.ts
@@ -1,19 +1,12 @@
-import { AxiosError } from "axios";
-import { AppError } from "../errors";
+import { handleErrors } from "../errors";
 import { User } from "../types";
 import api from "./api";
 
-export default async function listUsersServices(): Promise<User[]> {
+export default async function listUsersServices() {
   try {
     const { data } = await api<User[]>("/users");
     return data;
   } catch (error) {
-    if (error instanceof AxiosError) {
-      if (error.response?.status) {
-        throw new AppError(error.message, error.response?.status);
-      }
-      throw new AppError(error.message);
-    }
-    throw new AppError("An unexpected error occurred");
+    handleErrors(error);
   }
 }
