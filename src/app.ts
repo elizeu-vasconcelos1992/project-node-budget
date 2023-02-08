db1c189970bd8392938624131bddfab823b2d367
unknown <elizeucosta21@gmail.com>
2023-02-07 23:59:37 -0300

    feat: added users application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import "express-async-errors";
 import express from "express";
+import usersRoutes from "./routes";
+import checkErrors from "./errors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", usersRoutes);
+app.use(checkErrors);
 
 app.listen(3000, () => {
   console.log("Servidor executando");
